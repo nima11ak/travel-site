7f9b24f9939a10c3f844d70f102604e63be5c8b3
NIMA <nimaak98@yahoo.com>
2025-12-27 01:59:18 +0330

    create image  in post

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    # image =
-    # author = 
+    image = models.ImageField(upload_to='blog/',default='blog/abc.jpg')
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
         # tags = 
